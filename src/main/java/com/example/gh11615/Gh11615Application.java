69dd662ab28dc4c5198f1b6103ac4291cc9575d7
Eddú Meléndez <eddu.melendez@gmail.com>
2018-01-13 14:21:52 -0500

    Initial commit

---
+++ src/main/java/com/example/gh11615/Gh11615Application.java
@@ -0,0 +1,12 @@
+package com.example.gh11615;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gh11615Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gh11615Application.class, args);
+	}
+}
---
+++ src/test/java/com/example/gh11615/Gh11615ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.gh11615;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Gh11615ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
